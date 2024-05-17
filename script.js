dacd69587a3d18a7380d024a3c1371927eaeca40
Abdul Qadir Imran <abdulqadirimran01@gmail.com>
2024-05-17 13:19:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gameContainer = document.querySelector(".container"),
   cpuResult = document.querySelector(".cpu_result img"),
   result = document.querySelector(".result"),
   optionImages = document.querySelectorAll(".option_image");
-
+ 
 // Loop through each option image element
 optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
